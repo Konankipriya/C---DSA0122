ecb4a8fc267d21158e6e8e9893d7e61345155841
Konankipriya <112790443+Konankipriya@users.noreply.github.com>
2024-03-23 14:51:07 +0530

    Create 4 equal or not .cpp

---
+++ 4 equal or not .cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter the two numbers:"<<endl;
+	cin>>a>>b;
+	if(a==b)
+	{
+		cout<<a<<","<<b<<"two numbers are equal"<<endl;
+	}
+	else
+	{
+		cout<<a<<","<<b<<"not equal"<<endl;
+	}
+}
