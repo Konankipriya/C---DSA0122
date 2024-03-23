cf88f80445550d8e4bf97ea0cc3a06367be0ca95
Konankipriya <112790443+Konankipriya@users.noreply.github.com>
2024-03-23 14:45:17 +0530

    Create 3 average .cpp

---
+++ 3 average .cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y,z,average;
+	cout<<"enter the numbers: "<<endl;
+	cin>>x>>y>>z;
+	average=(x+y+z)/3;
+	cout<<"average of three numbers:"<<average<<endl;
+}
