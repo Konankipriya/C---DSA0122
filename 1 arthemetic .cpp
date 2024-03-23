036dfa759c2cbc1fb3034613b4aa49cfbe921ed2
Konankipriya <112790443+Konankipriya@users.noreply.github.com>
2024-03-23 14:40:28 +0530

    Create 1 arthemetic .cpp

---
+++ 1 arthemetic .cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,add,subtract,multiply,divide,madulo;
+	cout<<"enter the numbers:"<<"\n";
+	cin>>a>>b;
+	add= a+b;
+	subtract=a-b;
+	multiply=a*b;
+	divide=a/b;
+	madulo=a%b;
+	cout<<"\n"<< "addition of two numbers:"<<add;
+	cout<<"\n"<< "subtraction of two numbers:"<<subtract;
+	cout<<"\n"<< "multiplication of two numbers:"<<multiply;
+	cout<<"\n"<< "division of two numbers:"<<divide;
+	cout<<"\n"<< "madulo division of two numbers:"<<madulo;
+	
+}
